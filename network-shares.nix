f78b9fc326948f37b6fc34d66fa59dd4f594b879
Aurélien Mino <aurelien.mino@gmail.com>
2022-05-31 23:18:35 +0200

    Re-enable NFS on diskstation and remove global SMB share

--- network-shares.nix
+++ network-shares.nix
@@ -2,18 +2,8 @@
 
 {
   fileSystems."/mnt/diskstation" = {
-    device = "192.168.0.99:/volume1/";
+    device = "192.168.1.99:/volume1/";
     fsType = "nfs";
     options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s"];
-  };
-
-  fileSystems."/mnt/share/home" = {
-      device = "//192.168.0.99/homes";
-      fsType = "cifs";
-      options = let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
   };
 }
