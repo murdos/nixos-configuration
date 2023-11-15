c836c78f572b3261aac0e339824688a1d5f5a6e9
Aurélien Mino <aurelien.mino@gmail.com>
2023-11-15 21:52:34 +0100

    Enable GDM login with either password or fingerprint

--- desktop-gnome.nix
+++ desktop-gnome.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [ ./desktop-base.nix ];
@@ -13,6 +13,29 @@
   services.xserver = {
     desktopManager.gnome.enable = true;
     displayManager.gdm.enable = true;
+  };
+
+  # from https://github.com/NixOS/nixpkgs/issues/171136#issuecomment-1690517722
+  security.pam.services.login.fprintAuth = false;
+  # similarly to how other distributions handle the fingerprinting login
+  security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
+      text = ''
+        auth       required                    pam_shells.so
+        auth       requisite                   pam_nologin.so
+        auth       requisite                   pam_faillock.so      preauth
+        auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+        auth       optional                    pam_permit.so
+        auth       required                    pam_env.so
+        auth       [success=ok default=1]      ${pkgs.gnome.gdm}/lib/security/pam_gdm.so
+        auth       optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+        account    include                     login
+
+        password   required                    pam_deny.so
+
+        session    include                     login
+        session    optional                    ${pkgs.gnome.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+      '';
   };
 
   # Gnome Shell extensions install from Firefox
