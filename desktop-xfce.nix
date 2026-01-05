04388a3dc30969d6f96bda82cce2edbc890a7d66
Aurélien Mino <aurelien.mino@gmail.com>
2026-01-05 16:58:04 +0100

    chore(xfce): use new configuration keys for enabling sddm

--- desktop-xfce.nix
+++ desktop-xfce.nix
@@ -3,8 +3,8 @@
 {
   imports = [ ./desktop-base.nix ];
 
-  services.xserver = {
-    desktopManager.xfce.enable = true;
+  services = {
+    xserver.desktopManager.xfce.enable = true;
     displayManager.sddm.enable = true;
   };
 }
