a4241dfb2a9909c6f1e5a51e07c2677d27ec3aca
Aurélien Mino <aurelien.mino@gmail.com>
2026-05-31 21:47:00 +0200

    chore(network-shares): update nfs options for better compatibility and security

--- network-shares.nix
+++ network-shares.nix
@@ -2,8 +2,8 @@
 
 {
   fileSystems."/mnt/diskstation" = {
-    device = "192.168.1.99:/volume1/";
+    device = "192.168.1.99:/volume1";
     fsType = "nfs";
-    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s"];
+    options = ["noauto" "defaults" "hard" "retry=infinity" "timeo=300"];
   };
 }
